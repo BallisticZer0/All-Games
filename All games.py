1c91463a6cb9840b20bc912ca31c3a6ae0f4978b
BallisticZer0 <37849667+BallisticZer0@users.noreply.github.com>
2018-10-01 08:51:30 -0600

    Add files via upload

--- All games.py
+++ All games.py
@@ -1,23 +1,73 @@
-
 import Guess_the_numbers
-import ageguesser
+import age_guesser
+import Flip_a_Coin
+import tic_tac_tow
 import time
 
+game_list = ('Number guesser = 1\n' 'Age Guesser = 2\n' 'Coin flip = 3\n' 'Tic Tac Toe = 4\n' 'Quit = 8\n')
 
 def Games():
-    print("What game would you like to play?")
-    time.sleep(2)
-    game_list(['Number Guesser'],)
-    
-              
-               
-    
+    print("Alright what game do you want to play\n")
+    time.sleep(1)
+    print(game_list)
 
-while input("Shall we play a game?  [y|n]") == 'y':
+if input("Shall we play a game?  [y|n]") == 'y':
     Games()
 
 else:
-    print("Too Bad.")
+    print("Too bad.",quit())
 
 
 
+
+
+time.sleep(2)
+games = input("Type the the number next to the game you want to play__:\n\n")
+games = int(games)
+
+while(games<= 6 and games>=1): 
+    
+    if games == 1:
+        Guess_the_numbers.The_Number_guessing_game()
+        Games()
+
+        
+    elif games == 2:
+        age_guesser.AGE()
+
+    elif games == 3:
+        Flip_a_Coin.COIN()
+
+    elif games == 4:
+        tic_tac_tow.tic_tac_toe()
+
+    elif games == 5:
+        print()
+
+    elif games == 6:
+        print()
+
+    elif games == 7:
+        print()
+
+    else:
+        games == 8:
+            print("Well maybe next time. Good bye")
+            time.sleep(1)
+            quit()
+        
+        
+        
+
+
+
+# if input("Press Y if you want to you want to play again. OR press N to play a different game") == 'y':
+
+
+ #           else:
+#                print(game_list)
+
+
+
+
+    
